a3edca5b71772a3e86749958f8e84ab5a5fb83c4
Bestun Gardi <bestun95@yahoo.com>
2018-08-31 12:31:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function exactMatch (drivers, compare) {
   return drivers.filter( (driver) =>{
     let compare = false;
 key = compare.keys();
-    return driver[key] === compare[key];
+    return driver[key[0]] === compare[key[0]];
   });
 }
