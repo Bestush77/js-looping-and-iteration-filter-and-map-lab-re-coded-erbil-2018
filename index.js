ffb668a305018017c0fba691310be4d7ae1ba2ca
Bestun Gardi <bestun95@yahoo.com>
2018-08-31 12:00:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 function driversWithRevenueOver(driver, revenue){
   return driver.filter((driver)=> {return toString(driver.revenue > revenue)});
 }
+
+// Code your solution here:
+function driversWithRevenueOver(driver, revenue){
+  return driver.filter((driver)=> {return toString(driver.revenue > revenue)});
+}
