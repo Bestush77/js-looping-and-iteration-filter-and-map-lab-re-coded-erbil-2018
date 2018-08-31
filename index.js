2f26ecfa9af75467604e5b9b720dab7f6c25c631
Bestun Gardi <bestun95@yahoo.com>
2018-08-31 12:15:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ function driversWithRevenueOver(driver, revenue){
 
 // Code your solution here:
 function driverNamesWithRevenueOver(driver, revenue){
-  return driverNamesWithRevenueOver(driver,revenue).map((d)=> {return d.name})
+  return driversWithRevenueOver(drivers, revenue)
+    .map(function (driver) {
+      return driver.name;
+    });
   
 }
